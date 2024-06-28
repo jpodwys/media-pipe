eb89d177178621eeabf73bb7222ea17eafabe54c
Joe Podwys <joseph@podwys.com>
2024-06-28 10:43:44 -0600

    Cut out captureStream and add back blurring

--- Blur.js
+++ Blur.js
@@ -20,8 +20,8 @@ function Blur (mediaStream, width, height, frameRate, canvasElement) {
   let blurAmount = 10;
   let intervalId;
   // const canvasElement = document.createElement('canvas');
-  // canvasElement.width = width;
-  // canvasElement.height = height;
+  canvasElement.width = width;
+  canvasElement.height = height;
   const canvasCtx = canvasElement.getContext('2d');
   const selfieSegmentation = new SelfieSegmentation({locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
@@ -31,8 +31,8 @@ function Blur (mediaStream, width, height, frameRate, canvasElement) {
   });
   selfieSegmentation.onResults(render);
   const [ inputTrack ] = mediaStream.getVideoTracks();
-  const outputStream = canvasElement.captureStream(frameRate);
-  const [ outputTrack ] = outputStream.getVideoTracks()
+  // const outputStream = canvasElement.captureStream(frameRate);
+  // const [ outputTrack ] = outputStream.getVideoTracks()
   const videoElement = document.createElement('video');
   videoElement.setAttribute('autoplay', true);
   videoElement.setAttribute('style',
@@ -49,7 +49,7 @@ function Blur (mediaStream, width, height, frameRate, canvasElement) {
   const supportsFrameCallback = !!videoElement.requestVideoFrameCallback;
 
   function getOutputStream () {
-    return outputStream;
+    // return outputStream;
   }
 
   function toggleBlur () {
@@ -92,24 +92,24 @@ function Blur (mediaStream, width, height, frameRate, canvasElement) {
     // Draw the raw frame
     canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
 
-    // if (shouldBlur) {
-    //   // Make all pixels not in the segmentation mask transparent
-    //   canvasCtx.globalCompositeOperation = 'destination-atop';
-    //   canvasCtx.drawImage(results.segmentationMask, 0, 0, canvasElement.width, canvasElement.height);
+    if (shouldBlur) {
+      // Make all pixels not in the segmentation mask transparent
+      canvasCtx.globalCompositeOperation = 'destination-atop';
+      canvasCtx.drawImage(results.segmentationMask, 0, 0, canvasElement.width, canvasElement.height);
 
-    //   // Set the raw image as the background but squish it a little to hide the halo effect behind
-    //   // the foreground layer. This creates a nearly-perfect edge while the subject is centered.
-    //   // Because I shrunk the background image, I fill in the left, right, and top borders with
-    //   // duplicate, overlapping pixels. Overlapping blends the edges nicely.
-    //   canvasCtx.filter = `blur(${blurAmount}px)`;
-    //   canvasCtx.globalCompositeOperation = 'destination-over';
-    //   canvasCtx.drawImage(results.image, 0, 0, 30, canvasElement.height, 0, 0, 30, canvasElement.height);
-    //   canvasCtx.drawImage(results.image, canvasElement.width - 30, 0, canvasElement.width, canvasElement.height, canvasElement.width - 30, 0, canvasElement.width, canvasElement.height);
-    //   canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, 30, 0, 0, canvasElement.width, 30);
+      // Set the raw image as the background but squish it a little to hide the halo effect behind
+      // the foreground layer. This creates a nearly-perfect edge while the subject is centered.
+      // Because I shrunk the background image, I fill in the left, right, and top borders with
+      // duplicate, overlapping pixels. Overlapping blends the edges nicely.
+      canvasCtx.filter = `blur(${blurAmount}px)`;
+      canvasCtx.globalCompositeOperation = 'destination-over';
+      canvasCtx.drawImage(results.image, 0, 0, 30, canvasElement.height, 0, 0, 30, canvasElement.height);
+      canvasCtx.drawImage(results.image, canvasElement.width - 30, 0, canvasElement.width, canvasElement.height, canvasElement.width - 30, 0, canvasElement.width, canvasElement.height);
+      canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, 30, 0, 0, canvasElement.width, 30);
 
-    //   // Draw the shrunken background
-    //   canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height, 15, 10, canvasElement.width - 30, canvasElement.height - 10);
-    // }
+      // Draw the shrunken background
+      canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height, 15, 10, canvasElement.width - 30, canvasElement.height - 10);
+    }
 
     // Restore the context's blank state
     canvasCtx.restore();
